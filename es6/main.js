383953e2a27d4292d913a1bd4991eef79336fae8
Albert Fang <hr00090241@gmail.com>
2016-12-10 17:13:10 +0800

    add sass and image loader

--- es6/main.js
+++ es6/main.js
@@ -1,3 +1,4 @@
-import Point from './Point.js';
-var body = document.querySelector('body');
-body.textContent = 'Good point: ' + new Point(1, 23);
+import '../stylesheets/javascript30.scss';
+window.addEventListener('keydown', function (e) {
+  console.log(e);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,22 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'es6'),
-                query: {
-                  presets: 'es2015',
-                },
-            }
-        ]
+        loaders: [{
+            loader: 'babel-loader',
+            test: path.join(__dirname, 'es6'),
+            query: {
+                presets: 'es2015',
+            },
+        }, {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
+        }]
     },
     plugins: [
         // Avoid publishing files when compilation fails
