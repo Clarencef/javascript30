7ef149cedba1a6bf05b559969991c2f9961c5d4e
Albert Fang <hr00090241@gmail.com>
2016-12-27 00:28:46 +0800

    javascript30 - day8 completed

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: {
-        day1:      './es6/day1.js',
-        day2:      './es6/day2.js',
-        day3:      './es6/day3.js',
+        day1: './es6/day1.js',
+        day2: './es6/day2.js',
+        day3: './es6/day3.js',
         day4_day7: './es6/day4_day7.js',
-        day5:      './es6/day5.js',
-        day6:      './es6/day6.js'
+        day5: './es6/day5.js',
+        day6: './es6/day6.js',
+        day8: './es6/day8.js'
     },
     output: {
         path: __dirname + "/bundle",
@@ -16,31 +17,28 @@ module.exports = {
         publicPath: __dirname + "/bundle/"
     },
     module: {
-        loaders: [{
-            loader: 'babel-loader',
-            test: path.join(__dirname, 'es6'),
-            query: {
-                presets: 'es2015',
-            },
-        }, {
-            test: /\.scss$/,
-            loader: 'style!css!autoprefixer!sass'
-        }, {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            loaders: [
-                'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-            ]
-        }]
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: path.join(__dirname, 'es6'),
+                query: {
+                    presets: 'es2015'
+                }
+            }, {
+                test: /\.scss$/,
+                loader: 'style!css!autoprefixer!sass'
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false']
+            }
+        ]
     },
-    plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
-    ],
+    plugins: [// Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()],
     stats: {
         // Nice colored output
         colors: true
     },
     // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+    devtool: 'source-map'
 };
